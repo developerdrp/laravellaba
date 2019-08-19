c5677cd1b390b62bf4c5d9aa902e3ac493c4cd4d
develoeprdrp <drpoudel@outlook.com>
2019-08-19 18:09:40 +0545

    Laravel Project Startup and Basic Routes

---
+++ blog/resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+<h1>About Us</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minus, vel incidunt necessitatibus non eum numquam impedit porro dolores animi! Id commodi facilis repudiandae maiores ad eum iste, et odio.
+
+<br/>
+
+
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident nobis saepe ullam suscipit, consequuntur laboriosam maiores perferendis porro quasi, dignissimos, voluptas quisquam tempore numquam enim commodi vitae! Unde, quaerat tempore?
+
+<br/>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cumque pariatur aut nobis totam, sed accusantium, eveniet corporis a hic modi ducimus minus facilis amet error eligendi. At, minima tempora.
+</p>
+    
+</body>
+</html>
