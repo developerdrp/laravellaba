1a055518aa67c5deefde3dc04781579f6c35ee34
develoeprdrp <drpoudel@outlook.com>
2019-08-20 17:56:32 +0545

    Bootstrap Grid Simple Controller

---
+++ blog/resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+        Banner
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+        Menu
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-lg-6">
+          1
+         </div>
+         <div class="col-lg-6">
+         2
+         </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-3 col-sm-6 col-md-6">3</div>
+      <div class="col-lg-3 col-sm-6 col-md-6">4</div>
+      <div class="col-lg-3 col-sm-6 col-md-6">5</div>
+      <div class="col-lg-3 col-sm-6 col-md-6">6</div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-9">
+          7
+         </div>
+         <div class="col-lg-3">
+         8
+         </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+        Copyright
+        </div>
+    </div>
+
+</div><!--Container Close-->
+    
+</body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'homeController@index');
 
 Route::get('/hello', function ()
 {
